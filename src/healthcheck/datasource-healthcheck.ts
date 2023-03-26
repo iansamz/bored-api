a76971af8e0ce946d5817e4ddca2eb7ad48f5c85
Ian Mungai <iansam74@gmail.com>
2023-03-27 01:25:04 +0300

    fix: heathcheck

--- src/healthcheck/datasource-healthcheck.ts
+++ src/healthcheck/datasource-healthcheck.ts
@@ -1,11 +1,11 @@
-import { PrismaClient, Prisma } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 export const getDataSourceHealth = async (prismaClient?: PrismaClient) => {
 	try {
-		const prismaClientPingResult = await prismaClient?.$queryRaw<[{ 1: bigint }]>(
-			Prisma.sql(['SELECT 1'])
-		);
-		return Number(prismaClientPingResult?.[0][1]) === 1;
+		const prismaClientPingResult = await prismaClient?.$runCommandRaw({
+			ping: 1,
+		});
+		return prismaClientPingResult?.ok == 1;
 	} catch {
 		return false;
 	}
